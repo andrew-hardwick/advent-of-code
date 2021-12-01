2292f9a77eb1b635223f173a6a5123b33f28f03b
Andrew Hardwick <drew.hardwick@gmail.com>
2021-12-01 07:17:14 -0600

    slight rework 2021-01-02 and adding perf counter

--- 2021/01/p1.py
+++ 2021/01/p1.py
@@ -1,9 +1,14 @@
 # 2021/01/p1.py
 
+import time
+
+
 def parse_line(line):
 	return int(line.strip())
 
 def main(input_file):
+	pre = time.perf_counter()
+
 	with open(input_file, 'r') as f:
 		entries = map(parse_line, f.readlines())
 
@@ -15,7 +20,8 @@ def main(input_file):
 			count += 1
 		last = entry
 
-	print(count)
+	post = time.perf_counter()
+	print(count, 'in', (post - pre) * 1000, 'ms')
 
 if __name__ == '__main__':
 	main('test1.txt')
--- 2021/01/p2.py
+++ 2021/01/p2.py
@@ -1,23 +1,27 @@
 # 2021/01/p2.py
 
+import time
+
+
 def parse_line(line):
 	return int(line.strip())
 
 def main(input_file):
+	pre = time.perf_counter()
+
 	with open(input_file, 'r') as f:
 		entries = list(map(parse_line, f.readlines()))
 
 	last = 0
 	count = -1
 
-	for i, entry in enumerate(entries):
-		if i > 1:
-			window = sum(entries[i - 2 : i + 1])
-			if window > last:
+	for i, e in enumerate(entries):
+		if i > 2:
+			if entries[i] > entries[i - 3]:
 				count += 1
-			last = window
 
-	print(count)
+	post = time.perf_counter()
+	print(count, 'in', (post - pre) * 1000, 'ms')
 
 if __name__ == '__main__':
 	main('test1.txt')
