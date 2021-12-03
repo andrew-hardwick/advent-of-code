fa3feb2d6a5ff82f8f25b1f1b08f656f95ef56cf
Andrew Hardwick <drew.hardwick@gmail.com>
2021-12-03 07:08:37 -0600

    Dang copy paste errors. I blame the booster shot!

--- 2021/03/p2.py
+++ 2021/03/p2.py
@@ -20,43 +20,28 @@ def execute(input_file):
 	with open(input_file, 'r') as f:
 		entries = list(map(parse_line, f))
 
-	sums = [0] * len(entries[0])
-
 	ox_source = entries.copy()
 	scr_source = entries.copy()
-
-	print('')
 
 	for index, c in enumerate(entries[0]):
 		if len(ox_source) > 1:
 			# filter down based on this index
 			equal, most_used_bit = find_most_used_bit(ox_source, index)
 			if not equal:
-				print(index, len(ox_source), most_used_bit)
 				ox_source = [s for s in ox_source if s[index] == most_used_bit]
 			else:
-				print(index, len(ox_source), 1)
 				ox_source = [s for s in ox_source if s[index] == 1]
 		if len(scr_source) > 1:
 			# filter down based on this index
-			equal, most_used_bit = find_most_used_bit(ox_source, index)
+			equal, most_used_bit = find_most_used_bit(scr_source, index)
 			if not equal:
 				least_used_bit = 1 - most_used_bit
 				scr_source = [s for s in scr_source if s[index] == least_used_bit]
 			else:
-				ox_source = [s for s in ox_source if s[index] == 0]
+				scr_source = [s for s in scr_source if s[index] == 0]
 
 	a = convert_list_to_binary(ox_source[0])
 	b = convert_list_to_binary(scr_source[0])
-
-	print('')
-	print(len(ox_source))
-	print(len(scr_source))
-
-	print(ox_source[0])
-	print(scr_source[0])
-
-	print(a, b)
 
 	return a * b
 
