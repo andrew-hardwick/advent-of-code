0d8e7226ce300fd96add8bc98450a803b9d21766
Andrew Hardwick <drew.hardwick@gmail.com>
2021-12-07 06:31:47 -0600

    Updating template files

--- template/p2.py
+++ template/p2.py
@@ -6,8 +6,7 @@ import time
 def parse_line(line):
 	return line.strip()
 
-def main(input_file):
-	pre = time.perf_counter()
+def execute(input_file):
 
 	with open(input_file, 'r') as f:
 		entries = list(map(parse_line, f.readlines()))
@@ -15,7 +14,15 @@ def main(input_file):
 	# do the thing
 	result = 0
 
+	return result
+
+def main(input_file):
+	pre = time.perf_counter()
+
+	result = execute(input_file)
+
 	post = time.perf_counter()
+
 	print(result, 'in', (post - pre) * 1000, 'ms')
 
 if __name__ == '__main__':
