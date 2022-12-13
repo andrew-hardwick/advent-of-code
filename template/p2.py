3245bb443220dfcc045452112a2b9345604f58c3
Andrew Hardwick <drew.hardwick@gmail.com>
2022-12-13 16:00:35 -0600

    template, licence, gitignore and readme update

---
+++ template/p2.py
@@ -0,0 +1,31 @@
+# 20xx/xx/p2.py
+
+import time
+
+
+def parse_input(infn):
+	with open(infn, 'r') as f:
+		data = (str.strip(l) for l in f.readlines())
+
+	return data
+
+def execute(infn):
+	data = parse_input(infn)
+
+	# do the thing
+	result = 0
+
+	return result
+
+def main(infn):
+	pre = time.perf_counter()
+
+	result = execute(infn)
+
+	post = time.perf_counter()
+
+	print(result, 'in', (post - pre) * 1000, 'ms')
+
+if __name__ == '__main__':
+	main('test1.txt')
+	main('input.txt')
