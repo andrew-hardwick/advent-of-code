58c6941e119d098eb7aa85086e4b9cee84d35768
Andrew Hardwick <drew.hardwick@gmail.com>
2022-12-20 18:51:35 -0600

    day19 ongoing work

--- 2022/19/p1.py
+++ 2022/19/p1.py
@@ -8,114 +8,95 @@ import time
 def parse_line(line):
 	numbers = [int(i) for i in re.findall(r'\d+', line)]
 
-	ore_cost = [numbers[1], 0, 0]
-	cla_cost = [numbers[2], 0, 0]
-	obs_cost = [numbers[3], numbers[4], 0]
-	geo_cost = [numbers[5], 0, numbers[6]]
+	ore_cost = [numbers[1], 0, 0, 0]
+	cla_cost = [numbers[2], 0, 0, 0]
+	obs_cost = [numbers[3], numbers[4], 0, 0]
+	geo_cost = [numbers[5], 0, numbers[6], 0]
 
 	return ore_cost, cla_cost, obs_cost, geo_cost
 
 def parse_input(infn):
 	with open(infn, 'r') as f:
-		costs = [parse_line(l) for l in f.readlines()]
+		blueprints = [parse_line(l) for l in f.readlines()]
 
-	return costs
+	return blueprints
 
-def calculate_unit_benefit(costs):
+def able_to_purchase(blueprint, materials, choice):
+	return all(materials[i] >= blueprint[choice][i] for i in range(4))
 
-	# calculate how much each producer contributes to making 1 geode
+def time_step(materials, producers, time, time_delta):
+	return [materials[i] + (producers[i] * time_delta) for i in range(4)], time + time_delta
 
+def buy(materials, blueprint, choice):
+	return [materials[i] - blueprint[choice][i] for i in range(4)]
 
+def unable_to_produce(blueprint, producers, choice):
+	return any(producers[i] == 0 and blueprint[choice][i] > 0 for i in range(4))
 
-	return [2 ** i for i in range(4)]
-
-def calculate_mat_fractions(mats, costs):
-	return [[mats[j] / costs[i][j] if not costs[i][j] == 0 else 1 for j in range(4)] for i in range(4)]
-
-def simulate(costs, time_limit, purchase_order, max_geodes, r_geo_count):
-	producers = [1, 0, 0, 0]
-	materials = [0, 0, 0, 0]
-
-	purchase_index = 0
-
-	new_producer = -1
-
-	for minute in range(time_limit):
-		if purchase_index < len(purchase_order):
-			purchase = purchase_order[purchase_index]
-
-			if (time_limit - minute) *
-
-			#print('temp', materials, costs, purchase_index, purchase_order, purchase)
-
-			if all(materials[i] >= costs[purchase][i] for i in range(3)):
-				new_producer = purchase
-				materials = [materials[i] - costs[purchase][i] for i in range(3)] + [materials[3]]
-				purchase_index += 1
-
-		materials = [materials[i] + producers[i] for i in range(4)]
-
-		if (24 - minute) * r_geo_count + materials[3] < max_geodes:
-			#print('early exit')
-			return 0
-
-		#print(minute + 1, producers, new_producer, materials, costs[new_producer])
-
-		if new_producer > -1:
-			producers[new_producer] += 1
-			new_producer = -1
-
-
-	return materials[3]
-
-def ordering_valid(order):
-	index_1 = order.index(1)
-	index_2 = order.index(2)
-	index_3 = order.index(3)
-
-	return index_3 > index_2 and index_2 > index_1 and order[-1] == 3
-
-def optimize(costs, time_limit):
-	r_ore_cost, r_cla_cost, r_obs_cost, r_geo_cost = costs
-
+def choose_next(blueprint, time, time_limit, materials, producers, max_producers, choice, history):
 	max_geodes = 0
-	max_geode_producers = 2
+	best_child_history = []
 
-	best_working_geode_purchase = 0
+	if history == [1]:
+		print(history, choice, time)
+		print(materials, blueprint[choice])
+		print(able_to_purchase(blueprint, materials, choice))
+		print()
 
-	for r_ore_count in range(0, 2):
-		for r_cla_count in range(1, 5):
-			for r_obs_count in range(1, 4):
-				for r_geo_count in range(max_geode_producers, 5):
-					purchases = [1] * r_cla_count + [2] * r_obs_count + [3] * r_geo_count
+	if time > time_limit:
+		return materials[3], history
 
-					if len(purchases) > 12:
-						continue
+	# TODO make this do the necessary time steps without looping (and do an additional instead of below)
+	while not able_to_purchase(blueprint, materials, choice):
+		if time > time_limit:
+			return materials[3], history
+		materials, time = time_step(materials, producers, time, 1)
 
-					orderings = list(itertools.permutations(purchases, len(purchases)))
+	if time > time_limit:
+		return materials[3], history
 
-					max_geodes_per_run = 0
-					max_order = []
+	materials = buy(materials, blueprint, choice)
+	this_history = history.copy() + [choice]
+	materials, time = time_step(materials, producers, time, 1)
 
-					for order in orderings:
-						if ordering_valid(order):
-							#print('starting simulation', order, purchases)
-							geodes = simulate(costs, time_limit, [0] * r_ore_count + list(order), max_geodes, r_geo_count)
+	if time > time_limit:
+		return materials[3], history
 
-						if geodes > max_geodes_per_run:
-							max_order = order
-							max_geodes_per_run = geodes
+	producers[choice] += 1
 
-					if max_geodes_per_run > max_geodes:
-						max_geodes = max_geodes_per_run
-						max_geode_producers = r_geo_count
+	# decide on buying additional producers here
+	for next_choice in range(3, -1, -1):
+		if producers[next_choice] < max_producers[next_choice] and not unable_to_produce(blueprint, producers, next_choice):
+			geodes, child_history = choose_next(blueprint, time + 1, time_limit, materials.copy(), producers.copy(), max_producers, next_choice, this_history.copy())
 
-	return max_geodes
+			if geodes > max_geodes:
+				max_geodes = geodes
+				best_child_history = child_history
+
+	return max_geodes, best_child_history
+
+def optimize(blueprint, time_limit):
+	materials = [0, 0, 0, 0]
+	producers = [1, 0, 0, 0]
+
+	max_producers = [max(blueprint[p][m] for p in range(4)) for m in range(4)]
+	max_producers[3] = time_limit
+
+	# we can only buy choices 0 or 1 so we should only call for those
+	results = [choose_next(blueprint, 0, time_limit + 1, materials, producers, max_producers, choice, []) for choice in range(2)]
+
+	print(results, max_producers)
+
+	geodes, history = sorted(results, reverse=True)[0]
+
+	print(geodes, history)
+
+	return geodes
 
 def execute(infn, time_limit):
-	costs = parse_input(infn)
+	blueprints = parse_input(infn)
 
-	optimal_results = [optimize(cost, time_limit) for cost in costs]
+	optimal_results = [optimize(blueprint, time_limit) for blueprint in blueprints]
 
 	print(optimal_results)
 
