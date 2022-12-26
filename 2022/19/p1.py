97510947c562500412c15c34f2e2e1afed9fe031
Andrew Hardwick <drew.hardwick@gmail.com>
2022-12-26 08:23:02 -0600

    19 ongoing work

--- 2022/19/p1.py
+++ 2022/19/p1.py
@@ -1,19 +1,20 @@
-# 2022/19/p1.py
+# 20xx/xx/p1.py
 
-import itertools
+import math
 import re
 import time
 
+import numpy as np
 
 def parse_line(line):
 	numbers = [int(i) for i in re.findall(r'\d+', line)]
 
-	ore_cost = [numbers[1], 0, 0, 0]
-	cla_cost = [numbers[2], 0, 0, 0]
-	obs_cost = [numbers[3], numbers[4], 0, 0]
-	geo_cost = [numbers[5], 0, numbers[6], 0]
+	ore_bot = (np.array([numbers[1], 0, 0, 0]), np.array([1, 0, 0, 0]))
+	clay_bot = (np.array([numbers[2], 0, 0, 0]), np.array([0, 1, 0, 0]))
+	obsidian_bot = (np.array([numbers[3], numbers[4], 0, 0]), np.array([0, 0, 1, 0]))
+	geode_bot = (np.array([numbers[5], 0, numbers[6], 0]), np.array([0, 0, 0, 1]))
 
-	return ore_cost, cla_cost, obs_cost, geo_cost
+	return ore_bot, clay_bot, obsidian_bot, geode_bot
 
 def parse_input(infn):
 	with open(infn, 'r') as f:
@@ -21,109 +22,98 @@ def parse_input(infn):
 
 	return blueprints
 
-def able_to_purchase(blueprint, materials, choice):
-	return all(materials[i] >= blueprint[choice][i] for i in range(4))
-
 def time_step(materials, producers, time, time_delta):
+	return materials + (producers * time_delta), time + time_delta
+
 	return [materials[i] + (producers[i] * time_delta) for i in range(4)], time + time_delta
 
-def buy(materials, blueprint, choice):
-	return [materials[i] - blueprint[choice][i] for i in range(4)]
+def calculate_wait_time(blueprint, materials, producers, choice):
+	possible_times = [math.ceil(i) for i in ((blueprint[choice][0] - materials) / producers) if i >= 1]
 
-def unable_to_produce(blueprint, producers, choice):
-	return any(producers[i] == 0 and blueprint[choice][i] > 0 for i in range(4))
+	if len(possible_times) > 0:
+		return min(possible_times) + 1, possible_times
+
+	return 0, possible_times
 
 def choose_next(blueprint, time, time_limit, materials, producers, max_producers, choice, history):
-	max_geodes = 0
-	best_child_history = []
+	wait_time, possible_times = calculate_wait_time(blueprint, materials, producers, choice)
 
-	if history == [1, 1, 1]:
+	test_history = [1, 1]
+
+	if history == test_history:
+		print('choice', choice)
+		print(history)
+		print('cost', blueprint[choice][0])
+		print('materials', materials)
+		print('producers', producers)
 		print()
-		print(history, choice, time)
-		print(materials, blueprint[choice], producers)
-		print(able_to_purchase(blueprint, materials, choice))
+
+	if time + wait_time > time_limit:
+		return materials[3] + producers[3] * (time_limit - time)
+
+	materials, time = time_step(materials, producers, time, wait_time)
+
+	if history == test_history:
+		print('cost', blueprint[choice][0])
+		print('time', time)
+		print('possible_times', possible_times)
+		print('wait_time', wait_time)
+		print('materials', materials)
+		print('producers', producers)
 		print()
 
-	if time > time_limit:
-		return materials[3], history
+	materials = materials - blueprint[choice][0]
 
-	# if history == [1, 1, 1] and choice == 2:
-	# 	print('waiting for obsidian availability', time)
-	# 	print('materials', materials)
-	# 	print('producers', producers)
-	# 	print()
-
-	# TODO make this do the necessary time steps without looping (and do an additional instead of below)
-	while not able_to_purchase(blueprint, materials, choice):
-		if time > time_limit:
-			return materials[3], history
-		materials, time = time_step(materials, producers, time, 1)
-
-	# if history == [1, 1, 1] and choice == 2:
-	# 	print('time after waiting', time)
-	# 	print('materials', materials)
-	# 	print('producers', producers)
-	# 	print()
-
-	if time > time_limit:
-		return materials[3], history
-
-	materials = buy(materials, blueprint, choice)
-	this_history = history.copy() + [choice]
 	materials, time = time_step(materials, producers, time, 1)
 
-	if time > time_limit:
-		return materials[3], history
+	producers = producers + blueprint[choice][1]
 
-	producers[choice] += 1
+	if time >= time_limit:
+		return materials[3] + producers[3] * (time_limit - time)
 
-	# decide on buying additional producers here
-	for next_choice in range(3, -1, -1):
-		if this_history == [1, 1, 1] and next_choice == 2:
-			print('first obsidian:', time)
-		if producers[next_choice] < max_producers[next_choice] and not unable_to_produce(blueprint, producers, next_choice):
-			geodes, child_history = choose_next(blueprint, time, time_limit, materials.copy(), producers.copy(), max_producers, next_choice, this_history.copy())
+	if history == test_history:
+		print('time', time)
+		print('materials', materials)
+		print('producers', producers)
+		print()
 
-			if geodes > max_geodes:
-				max_geodes = geodes
-				best_child_history = child_history
+	non_max_robots = set([i for i in range(4) if producers[i] < max_producers[i]])
+	able_to_buy_robots = set([i for i in range(4) if all(not producers[j] == 0 or blueprint[i][0][j] == 0 for j in range(4))])
 
-	return max_geodes, best_child_history
+	robots_to_buy = non_max_robots & able_to_buy_robots
+
+	next_history = history + [choice]
+
+	child_geodes = [choose_next(blueprint, time, time_limit, materials.copy(), producers.copy(), max_producers, next_robot, next_history) for next_robot in robots_to_buy]
+
+	return max(child_geodes)
 
 def optimize(blueprint, time_limit):
-	materials = [0, 0, 0, 0]
-	producers = [1, 0, 0, 0]
+	materials = np.array([0, 0, 0, 0])
+	producers = np.array([1, 0, 0, 0])
 
-	max_producers = [max(blueprint[p][m] for p in range(4)) for m in range(4)]
+	max_producers = np.array([max((blueprint[r][0][i] for r in range(4))) for i in range(4)])
 	max_producers[3] = time_limit
 
-	# we can only buy choices 0 or 1 so we should only call for those
-	results = [choose_next(blueprint, 0, time_limit + 1, materials, producers.copy(), max_producers, choice, []) for choice in range(2)]
+	for i in range(2):
+		next_bot = 1 - i
 
-	print('exploration complete')
-
-	print(results, max_producers)
-
-	geodes, history = sorted(results, reverse=True)[0]
-
-	print(geodes, history)
+		geodes = choose_next(blueprint, 0, time_limit, materials, producers, max_producers, next_bot, [])
 
 	return geodes
 
 def execute(infn, time_limit):
 	blueprints = parse_input(infn)
 
-	result_1 = optimize(blueprints[0], time_limit)
-	print(result_1)
-	return
+	test = optimize(blueprints[0], time_limit)
 
 	return '??'
 
-	optimal_results = [optimize(blueprint, time_limit) for blueprint in blueprints]
+	geodes = [optimize(blueprint, time_limit) for blueprint in blueprints]
 
-	print(optimal_results)
+	quality_levels = [i * g for i, g in enumerate(geodes)]
 
-	return '??'
+	return sum(quality_levels)
 
 def main(infn, time_limit):
 	pre = time.perf_counter()
@@ -136,4 +126,4 @@ def main(infn, time_limit):
 
 if __name__ == '__main__':
 	main('test1.txt', 24)
-	#main('input.txt', 24)
+#	main('input.txt')
