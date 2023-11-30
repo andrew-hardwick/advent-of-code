231820884f322cb9bfe41fb5386de4092f7c2bd4
Drew Hardwick <drew.hardwick@gmail.com>
2023-11-29 22:00:48 -0600

    2015-12 done

--- 2015/12/p1.py
+++ 2015/12/p1.py
@@ -1,31 +1,67 @@
 # 20xx/xx/p1.py
 
+import json
 import time
 
 
-def parse_input(infn):
-	with open(infn, 'r') as f:
-		data = (str.strip(l) for l in f.readlines())
+def parse_input(
+        infn):
+    with open(infn, 'r') as f:
+        data = json.loads(f.read())
 
-	return data
+    return data
 
-def execute(infn):
-	data = parse_input(infn)
 
-	# do the thing
-	result = 0
+def sum_numbers_in_list(
+        source,
+        ignore_red):
+    return sum(sum_numbers_in_item(item, ignore_red) for item in source)
 
-	return result
 
-def main(infn):
-	pre = time.perf_counter()
+def sum_numbers_in_dict(
+        source,
+        ignore_red):
+    if ignore_red and 'red' in source.values():
+        return 0
 
-	result = execute(infn)
+    return sum(sum_numbers_in_item(source[key], ignore_red) for key in source.keys())
 
-	post = time.perf_counter()
 
-	print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+def sum_numbers_in_item(
+        item,
+        ignore_red):
+    if isinstance(item, list):
+        return sum_numbers_in_list(item, ignore_red)
+    if isinstance(item, dict):
+        return sum_numbers_in_dict(item, ignore_red)
+    if isinstance(item, int):
+        return item
+    if isinstance(item, str):
+        return 0
+
+
+def execute(
+        infn,
+        ignore_red):
+    data = parse_input(infn)
+
+    result = sum_numbers_in_item(data, ignore_red)
+
+    return result
+
+
+def main(
+        infn,
+        ignore_red):
+    pre = time.perf_counter()
+
+    result = execute(infn, ignore_red)
+
+    post = time.perf_counter()
+
+    print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+
 
 if __name__ == '__main__':
-	main('test1.txt')
-	main('input.txt')
+    main('input.txt', False)
+
