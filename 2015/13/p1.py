43fb27f81bb102eff6a2e6e78b6ed5e3920f7b42
Drew Hardwick <drew.hardwick@gmail.com>
2023-11-29 22:25:15 -0600

    2015-13 done

--- 2015/13/p1.py
+++ 2015/13/p1.py
@@ -1,31 +1,84 @@
 # 20xx/xx/p1.py
 
+import itertools
 import time
 
 
-def parse_input(infn):
-	with open(infn, 'r') as f:
-		data = (str.strip(l) for l in f.readlines())
+def parse_line(
+        line):
+    line = line.strip().replace('gain ', '').replace('lose ', '-')
+    line = line.replace(' would', '')
+    line = line.replace(' happiness units by sitting next to', '')
+    line = line.replace('.', '')
 
-	return data
+    split_line = line.split(' ')
 
-def execute(infn):
-	data = parse_input(infn)
+    return split_line[0], split_line[2], int(split_line[1])
 
-	# do the thing
-	result = 0
 
-	return result
+def calculate_total_happiness(
+        seating_chart,
+        happiness_dict):
+    pairs = list(zip(seating_chart[:-1], seating_chart[1:]))
+    pairs.append((seating_chart[-1], seating_chart[0]))
 
-def main(infn):
-	pre = time.perf_counter()
+    return sum(happiness_dict[pair] for pair in pairs)
 
-	result = execute(infn)
 
-	post = time.perf_counter()
+def parse_input(
+        infn):
+    with open(infn, 'r') as f:
+        data = [parse_line(line) for line in f.readlines()]
 
-	print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+    people = list(set(p1 for p1, _0, _1 in data))
+
+    happiness_dict = {}
+
+    for p_1, p_2, gain in data:
+        if (p_1, p_2) not in happiness_dict.keys():
+            happiness_dict[(p_1, p_2)] = 0
+        if (p_2, p_1) not in happiness_dict.keys():
+            happiness_dict[(p_2, p_1)] = 0
+
+        happiness_dict[(p_1, p_2)] += gain
+        happiness_dict[(p_2, p_1)] += gain
+
+    return people, happiness_dict
+
+
+def execute(
+        infn,
+        add_nullo):
+    people, happiness_dict = parse_input(infn)
+
+    if add_nullo:
+        for person in people:
+            happiness_dict[(person, 'nullo')] = 0
+            happiness_dict[('nullo', person)] = 0
+
+        people.append('nullo')
+
+    perms = itertools.permutations(people)
+
+    total_happiness_all_perms = (calculate_total_happiness(perm, happiness_dict) for perm in perms)
+
+    result = max(total_happiness_all_perms)
+
+    return result
+
+
+def main(
+        infn,
+        add_nullo):
+    pre = time.perf_counter()
+
+    result = execute(infn, add_nullo)
+
+    post = time.perf_counter()
+
+    print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+
 
 if __name__ == '__main__':
-	main('test1.txt')
-	main('input.txt')
+    main('input.txt', False)
+
