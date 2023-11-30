a18fd7d28f0abb83dd4ca794ef4941bcadef9964
Drew Hardwick <drew.hardwick@gmail.com>
2023-11-29 21:28:43 -0600

    2015-10 reimpl

--- 2015/10/p1.py
+++ 2015/10/p1.py
@@ -3,29 +3,62 @@
 import time
 
 
-def parse_input(infn):
-	with open(infn, 'r') as f:
-		data = (str.strip(l) for l in f.readlines())
+def parse_input(
+        infn):
+    with open(infn, 'r') as f:
+        data = [int(c) for c in f.read()]
 
-	return data
+    return data
 
-def execute(infn):
-	data = parse_input(infn)
 
-	# do the thing
-	result = 0
+def iterate(
+        source):
+    result = []
 
-	return result
+    current = source[0]
+    count = 1
 
-def main(infn):
-	pre = time.perf_counter()
+    for e in source[1:]:
+        if e == current:
+            count += 1
+        else:
+            result.append(count)
+            result.append(current)
 
-	result = execute(infn)
+            current = e
+            count = 1
 
-	post = time.perf_counter()
+    result.append(count)
+    result.append(current)
 
-	print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+    return result
+
+
+def execute(
+        infn,
+        iteration_count):
+    data = parse_input(infn)
+
+    for _ in range(iteration_count):
+        data = iterate(data)
+
+    result = len(data)
+
+    return result
+
+
+def main(
+        infn,
+        iteration_count):
+    pre = time.perf_counter()
+
+    result = execute(infn, iteration_count)
+
+    post = time.perf_counter()
+
+    print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+
 
 if __name__ == '__main__':
-	main('test1.txt')
-	main('input.txt')
+    main('input.txt', 40)
+
