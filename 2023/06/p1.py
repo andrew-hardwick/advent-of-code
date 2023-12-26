4e5af268abfa8c6ff675447eb51ca91cf341a925
Drew Hardwick <drew.hardwick@gmail.com>
2023-12-26 10:59:49 -0600

    2023 day 6

--- 2023/06/p1.py
+++ 2023/06/p1.py
@@ -1,37 +1,67 @@
 # 20xx/xx/p1.py
 
+import math
 import time
 
 
-def parse_input(
-        infn):
-    with open(infn, 'r') as f:
-        data = (str.strip(line) for line in f.readlines())
+def parse_numbers_from_line(
+		line):
+	number_source = line.split(':')[1].strip()
 
-    return data
+	while '  ' in number_source:
+		number_source = number_source.replace('  ', ' ')
+
+	return [int(n) for n in number_source.split(' ')]
+
+def parse_input(
+		infn):
+	with open(infn, 'r') as f:
+		time_source, distance_source = [str.strip(line) for line in f.readlines()]
+
+	times = parse_numbers_from_line(time_source)
+	distances = parse_numbers_from_line(distance_source)    
+
+	return zip(times, distances)
+
+
+def find_record_breaking_count(
+		time_limit,
+		distance):
+	discriminant = time_limit ** 2 - 4 * distance
+
+	root_discriminant = discriminant ** 0.5
+
+	lower_bound = math.floor((time_limit - root_discriminant) / 2)
+	upper_bound = math.ceil((time_limit + root_discriminant) / 2)
+
+	return upper_bound - lower_bound - 1
 
 
 def execute(
-        infn):
-    data = parse_input(infn)
+		infn):
+	data = parse_input(infn)
 
-    # do the thing
-    result = 0
+	result = 1
 
-    return result
+	for time_limit, distance in data:
+		count = find_record_breaking_count(time_limit, distance)
+
+		result *= count
+
+	return result
 
 
 def main(
-        infn):
-    pre = time.perf_counter()
+		infn):
+	pre = time.perf_counter()
 
-    result = execute(infn)
+	result = execute(infn)
 
-    post = time.perf_counter()
+	post = time.perf_counter()
 
-    print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+	print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
 
 
 if __name__ == '__main__':
-    main('input.txt')
+	main('input.txt')
 
--- 2023/06/p2.py
+++ 2023/06/p2.py
@@ -1,37 +1,38 @@
 # 20xx/xx/p2.py
 
+from p1 import parse_input, find_record_breaking_count
+
 import time
 
 
-def parse_input(
-        infn):
-    with open(infn, 'r') as f:
-        data = (str.strip(line) for line in f.readlines())
-
-    return data
+def condense_number(
+		source):
+	return int(''.join([str(i) for i in source]))
 
 
 def execute(
-        infn):
-    data = parse_input(infn)
+		infn):
+	data = parse_input(infn)
 
-    # do the thing
-    result = 0
+	times, distances = zip(*data)
 
-    return result
+	time_limit = condense_number(times)
+	distance = condense_number(distances)
+
+	return find_record_breaking_count(time_limit, distance)
 
 
 def main(
-        infn):
-    pre = time.perf_counter()
+		infn):
+	pre = time.perf_counter()
 
-    result = execute(infn)
+	result = execute(infn)
 
-    post = time.perf_counter()
+	post = time.perf_counter()
 
-    print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
+	print(result, 'in', '{:.2f}'.format((post - pre) * 1000), 'ms')
 
 
 if __name__ == '__main__':
-    main('input.txt')
+	main('input.txt')
 
